db3b034e722ad22c17699f84606ce06fcc87f9ce
Aathif Mahir <aathifmahir@gmail.com>
2025-10-10 02:22:15 +0530

    Add BenchmarkResults class to store performance metrics for widgets and builds

---
+++ benchmark/lib/models/benchmark_results.dart
@@ -0,0 +1,7 @@
+/// Holds benchmark results for all test categories
+class BenchmarkResults {
+  final Map<String, int> widgetPerformance = {};
+  final Map<String, int> buildPerformance = {};
+  final Map<String, int> memoryPerformance = {};
+  final Map<String, int> selectiveRebuildPerformance = {};
+}
