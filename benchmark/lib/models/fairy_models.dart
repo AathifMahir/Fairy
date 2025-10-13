bd9cbfea44a6854a660a5253874205bb179b018b
Aathif Mahir <aathifmahir@gmail.com>
2025-10-13 21:35:52 +0530

    Remove unnecessary parent references from ObservableProperty initializations in ViewModels

--- benchmark/lib/models/fairy_models.dart
+++ benchmark/lib/models/fairy_models.dart
@@ -6,7 +6,7 @@ class FairyCounterViewModel extends ObservableObject {
   late final RelayCommand incrementCommand;
 
   FairyCounterViewModel() {
-    counter = ObservableProperty(0, parent: this);
+    counter = ObservableProperty(0);
     incrementCommand = RelayCommand(() {
       counter.value++;
     }, parent: this);
@@ -20,9 +20,9 @@ class FairyMultiPropertyViewModel extends ObservableObject {
   late final ObservableProperty<int> property3;
 
   FairyMultiPropertyViewModel() {
-    property1 = ObservableProperty(0, parent: this);
-    property2 = ObservableProperty(0, parent: this);
-    property3 = ObservableProperty(0, parent: this);
+    property1 = ObservableProperty(0);
+    property2 = ObservableProperty(0);
+    property3 = ObservableProperty(0);
   }
   
   /// Helper for ONE-WAY binding: triggers global notifications
